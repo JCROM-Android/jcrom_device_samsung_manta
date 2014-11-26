4354f20e1f8b07d5a012926e6204507dc13a3887
Akihiro Maeda <sola.1980.a@gmail.com>
2014-11-26 20:29:21 +0900

    Change PRODUCT_BRAND and PRODUCT_MODEL for products

--- full_manta.mk
+++ full_manta.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2011 The Android Open Source Project
+#
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,14 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#
-# This file is the build configuration for a full Android
-# build for manta hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps). Except for a few implementation
-# details, it only fundamentally contains two inherit-product
-# lines, full and manta, hence its name.
 #
 
 # Live Wallpapers
@@ -39,9 +32,9 @@ $(call inherit-product, device/samsung/manta/device.mk)
 
 PRODUCT_NAME := full_manta
 PRODUCT_DEVICE := manta
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Manta
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 10
+PRODUCT_MANUFACTURER := samsung
 PRODUCT_RESTRICT_VENDOR_FILES := owner path
 
 #AOSP
