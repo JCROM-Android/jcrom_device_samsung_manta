3f22bb1788ddbb0e4e7f7fc3246eaf335b4cb735
Akihiro Maeda <sola.1980.a@gmail.com>
2014-11-26 21:30:14 +0900

    Add the definition of AOSP build

--- aosp_manta.mk
+++ aosp_manta.mk
@@ -13,6 +13,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/samsung/manta/full_manta.mk)
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+        LiveWallpapers \
+        LiveWallpapersPicker \
+        MagicSmokeWallpapers \
+        VisualizationWallpapers \
+        librs_jni
+
+PRODUCT_PROPERTY_OVERRIDES := \
+        net.dns1=8.8.8.8 \
+        net.dns2=8.8.4.4
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, device/samsung/manta/device.mk)
 
 PRODUCT_NAME := aosp_manta
+PRODUCT_DEVICE := manta
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on Manta
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_RESTRICT_VENDOR_FILES := owner path
+
+#AOSP
+$(call inherit-product-if-exists, vendor/aosp/samsung/manta/aosp.mk)
+$(call inherit-product, device/aosp/common/aosp.mk)
